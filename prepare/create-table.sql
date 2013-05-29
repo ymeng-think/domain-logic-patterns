2f5d309ad2e6f444591e13a57cce6f29bffd2781
Meng Yu <ymeng@thoughtworks.com>
2013-05-29 16:39:39 +0800

    [ymeng] Write script to create tables

---
+++ prepare/create-table.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE domain_logic_patterns;
+
+USE domain_logic_patterns;
+
+CREATE TABLE product (ID int primary key, name varchar(30), type varchar(30));
+CREATE TABLE contract (ID int primary key, product int, revenue decimal, date_signed date);
+CREATE TABLE revenue_recognition (contract int, amout decimal, recognized_on date, PRIMARY KEY(contract, recognized_on));
+
+-- SHOW databases
+-- SHOW tables
+-- DESC [table_name]
