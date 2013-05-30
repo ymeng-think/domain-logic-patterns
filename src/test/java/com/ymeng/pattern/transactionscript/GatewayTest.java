c6f81f5fce07e85290384328a0acaf99e653eb09
Meng Yu <ymeng@thoughtworks.com>
2013-05-30 09:33:18 +0800

    [ymeng] Add more cases for Gateway

--- src/test/java/com/ymeng/pattern/transactionscript/GatewayTest.java
+++ src/test/java/com/ymeng/pattern/transactionscript/GatewayTest.java
@@ -15,23 +15,32 @@ import static org.hamcrest.Matchers.is;
 public class GatewayTest extends DatabaseTest {
 
     private Gateway gateway;
+    private Recognition recognition;
 
     @Override
     public void setUp() throws Exception {
         super.setUp();
 
-        Recognition recognition = new Recognition(connection);
-        recognition.insert(1, 100, date(2012, 2, 1));
-
+        recognition = new Recognition(connection);
         gateway = new Gateway(connection);
     }
 
     @Test
     public void should_find_recognitions_from_database() throws SQLException {
+        recognition.insert(1, 100, date(2012, 2, 1));
+        recognition.insert(2, 100, date(2012, 2, 2));
+
         ResultSet result = gateway.findRecognitionsFor(1, date(2012, 2, 1));
 
         assertThat(result.next(), is(true));
         assertThat(result.getDouble(1), is(100.0));
+        assertThat(result.next(), is(false));
+    }
+
+    @Test
+    public void should_NOT_find_any_recognitions_when_key_is_NOT_match() throws SQLException {
+        ResultSet result = gateway.findRecognitionsFor(1, date(2012, 2, 1));
+
         assertThat(result.next(), is(false));
     }
 
