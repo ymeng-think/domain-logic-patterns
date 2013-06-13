2b0d8baea28d385bfb4de90435f9f5a078d0b2d4
Meng Yu <ymeng@thoughtworks.com>
2013-06-13 19:17:47 +0800

    [ymeng] Encapsulate extraction of year,month and day during compare date object

--- src/test/java/com/ymeng/matcher/DateEqualMatcher.java
+++ src/test/java/com/ymeng/matcher/DateEqualMatcher.java
@@ -1,10 +1,12 @@
 package com.ymeng.matcher;
 
 import org.hamcrest.Description;
-import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
 
+import java.util.Calendar;
 import java.util.Date;
+
+import static java.util.Calendar.*;
 
 public class DateEqualMatcher extends TypeSafeMatcher<Date> {
 
@@ -25,8 +27,40 @@ public class DateEqualMatcher extends TypeSafeMatcher<Date> {
     }
 
     private static boolean compare(Date date1, Date date2) {
-        return date1.getYear() == date2.getYear() &&
-                date1.getMonth() == date2.getMonth() &&
-                date1.getDate() == date2.getDate();
+        DateWrapper d1 = DateWrapper.wrap(date1);
+        DateWrapper d2 = DateWrapper.wrap(date2);
+        return d1.equals(d2);
+    }
+
+    private static class DateWrapper {
+
+        private final int year;
+        private final int month;
+        private final int day;
+
+        private DateWrapper(Date date) {
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(date);
+            year = calendar.get(YEAR);
+            month = calendar.get(MONTH);
+            day = calendar.get(DAY_OF_MONTH);
+        }
+
+        public static DateWrapper wrap(Date date) {
+            return new DateWrapper(date);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+
+            DateWrapper that = (DateWrapper) o;
+            return day == that.day && month == that.month && year == that.year;
+        }
     }
 }
